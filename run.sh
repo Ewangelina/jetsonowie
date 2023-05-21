46e8b05399e2a31f2358e7a6aa34fd25de28f8a9
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-05-21 20:45:34 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+xhost +local:
+sudo docker run \
+       -it \
+       --rm \
+       --runtime nvidia \
+       --network host \
+       --device /dev/video0:/dev/video0:mrw \
+       -e DISPLAY=$DISPLAY \
+       -e LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 \
+       -v /tmp/.X11-unix/:/tmp/.X11-unix \
+	-v $(pwd)/snippets:/yolov5/snippets \
+       yolov5 python3.8 detect.py --source 0 --class 3
